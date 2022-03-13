10dd3f55ba757e283d034e9b1dd0226eab2ec815
sw1227 <32032108+sw1227@users.noreply.github.com>
2022-03-13 10:59:02 +0900

    fix: config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/kbg-map" : "",
+  trailingSlash: true,
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
 }
 
 module.exports = nextConfig
